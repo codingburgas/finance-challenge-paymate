c6982664d864b45d74e6eb2f74ef0f898954db6e
Stoyan Hristov <SPHristov22@codingburgas.bg>
2024-10-21 20:01:08 +0300

    Set raylib

---
+++ app/PM/main.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+
+
+int main()
+{
+   
+    const int screenWidth = 900;
+    const int screenHeight = 1080;
+
+    InitWindow(screenWidth, screenHeight, "PM");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("PayMate", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+
+    }
+    CloseWindow();
+}
+
+    
